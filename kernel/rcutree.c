8816bfb95f394c13b9e775cfabca2900c2122e03
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-06-21 13:00:57 -0700

    nohz_full: Add per-CPU idle-state tracking
    This commit adds the code that updates the rcu_dyntick structure's
    new fields to track the per-CPU idle state based on interrupts and
    transitions into and out of the idle loop (NMIs are ignored because NMI
    handlers cannot cleanly read out the time anyway).  This code is similar
    to the code that maintains RCU's idea of per-CPU idleness, but differs
    in that RCU treats CPUs running in user mode as idle, where this new
    code does not.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Acked-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -416,6 +416,7 @@ void rcu_idle_enter(void)
 
 	local_irq_save(flags);
 	rcu_eqs_enter(false);
+	rcu_sysidle_enter(&__get_cpu_var(rcu_dynticks), 0);
 	local_irq_restore(flags);
 }
 EXPORT_SYMBOL_GPL(rcu_idle_enter);
@@ -487,6 +488,7 @@ void rcu_irq_exit(void)
 		trace_rcu_dyntick("--=", oldval, rdtp->dynticks_nesting);
 	else
 		rcu_eqs_enter_common(rdtp, oldval, true);
+	rcu_sysidle_enter(rdtp, 1);
 	local_irq_restore(flags);
 }
 
@@ -555,6 +557,7 @@ void rcu_idle_exit(void)
 
 	local_irq_save(flags);
 	rcu_eqs_exit(false);
+	rcu_sysidle_exit(&__get_cpu_var(rcu_dynticks), 0);
 	local_irq_restore(flags);
 }
 EXPORT_SYMBOL_GPL(rcu_idle_exit);
@@ -628,6 +631,7 @@ void rcu_irq_enter(void)
 		trace_rcu_dyntick("++=", oldval, rdtp->dynticks_nesting);
 	else
 		rcu_eqs_exit_common(rdtp, oldval, true);
+	rcu_sysidle_exit(rdtp, 1);
 	local_irq_restore(flags);
 }
 
