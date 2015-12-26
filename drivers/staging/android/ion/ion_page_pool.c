997ffc4b1ea38e0d6c8ba856fbd80f24d8c65d50
sakindia123 <sa6915@gmail.com>
2015-12-26 08:39:42 +0530

    Revert "staging: android: Reduce allocation time from system heap"
    This reverts commit d6b5aeb3f0bb78906189a1a2b41d55074ecded43.

--- drivers/staging/android/ion/ion_page_pool.c
+++ drivers/staging/android/ion/ion_page_pool.c
@@ -90,13 +90,12 @@ struct page *ion_page_pool_alloc(struct ion_page_pool *pool)
 
 	BUG_ON(!pool);
 
-	if (mutex_trylock(&pool->mutex)) {
-		if (pool->high_count)
-			page = ion_page_pool_remove(pool, true);
-		else if (pool->low_count)
-			page = ion_page_pool_remove(pool, false);
-		mutex_unlock(&pool->mutex);
-	}
+	mutex_lock(&pool->mutex);
+	if (pool->high_count)
+		page = ion_page_pool_remove(pool, true);
+	else if (pool->low_count)
+		page = ion_page_pool_remove(pool, false);
+	mutex_unlock(&pool->mutex);
 
 	return page;
 }
